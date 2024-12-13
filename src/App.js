f6322598a0862d0403bddcf92f991a8947c3ecd1
tahakhanabdalli <tahakhanabdalli@gmail.com>
2024-12-13 19:03:46 +0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import LandingRoutes from "./routes";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <LandingRoutes />
+    </div>
+  );
+}
+
+export default App;
